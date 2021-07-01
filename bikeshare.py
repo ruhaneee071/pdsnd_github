f72e638443ac7fda22adeb038efe2d89687ce165
ruhaneee071 <ruhaneee071@gmail.com>
2021-07-01 10:50:10 +1000

    Code modified for the function display_csv_data

--- bikeshare.py
+++ bikeshare.py
@@ -210,14 +210,14 @@ def user_stats(df):
     user_types_counts= df['User Type'].value_counts()
     print('\nThe counts of user types are: {}\n'.format(user_types_counts))
 
-    # Display counts of gender. This code will check if 'Gender' column is present in the csv file.
+    # Display counts of gender
     try:
         gender_counts = df['Gender'].value_counts()
         print('\nThe counts of each gender are: {}\n'.format(gender_counts))
     except:
         print("\nThere is no 'Gender' specific data for this city.\n")
 
-    # Display earliest, most recent, and most common year of birth. This code will check if 'Birth Year' column is present in the csv file.
+    # Display earliest, most recent, and most common year of birth
     try:
         earliest_birth_year= int(df['Birth Year'].min())
         recent_birth_year = int(df['Birth Year'].max())
@@ -230,41 +230,18 @@ def user_stats(df):
     print('-'*40)
 
 
+def display_csv_data(df, city):
+#Displays raw data (5 rows) from the city selected by the user.
+    print('*'*120)
+    
+    row_index = 0
+    while True:
+        disp_data = input("\nDo you want to see 5 rows of data for the city: {}? Yes or No:\n".format(city)).lower()
+        if disp_data != 'yes':
+            return
+        print(df.iloc[row_index:row_index+5])
+        row_index = row_index + 5
 
-def display_csv_data(df):
-    """Displays 5 rows of data from the csv file for the selected city.
-    Args:
-        param1 (df): The data frame you want to work with.
-    Returns:
-        None.
-    """
-    USER_RESPONSE_LIST = ['yes', 'no']
-    row_data = ''
-    index = 0
-    while row_data not in USER_RESPONSE_LIST:
-        print("\nDo you want to view the raw data?")
-        print("\nAccepted responses:\nYes or yes\nNo or no")
-        row_data = input().lower()
-        #the raw data from the df is displayed if user opts for it
-        if row_data == "yes":
-            print(df.head())
-        elif row_data not in USER_RESPONSE_LIST:
-            print("\nPlease select your input as \nYes or yes\nNo or no.")
-            print("\nRestarting...\n")
-
-    #Additional while loop if the user want to continue viewing data
-    while row_data == 'yes':
-        print("Do you want to view more raw data?")
-        print("\nAccepted responses:\nYes or yes\nNo or no")
-        index += 5
-        row_data = input().lower()
-        # This displays next 5 rows of data
-        if row_data == "yes":
-             print(df[index:index+5])
-        elif row_data != "yes":
-             break
-
-    print('-'*40)
 
 def main():
     while True:
@@ -274,7 +251,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        display_csv_data(df)
+        display_csv_data(df,city)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
